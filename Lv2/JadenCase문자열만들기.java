cfe4e4990b5474b74f5f5d6515561d9518f93b3a
MinWook6457 <minuk6457@gmail.com>
2024-01-03 14:01:30 +0900

    Solved Programmers Lv2

---
+++ Lv2/JadenCase문자열만들기.java
@@ -0,0 +1,37 @@
+package Lv2;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+// 3people unFollowed me
+
+public class JadenCase문자열만들기 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.nextLine();
+		
+		str = str.toLowerCase(); // 전부 소문자로 바꾸기
+		
+		System.out.println(str);
+		
+		StringTokenizer st = new StringTokenizer(str, " ", true);
+		
+		StringBuilder sb = new StringBuilder(); // 문자열을 연결해야 하므로 변경 가능한 문자열을 만드는 StringBuilder 선언
+		
+		while (st.hasMoreTokens()) {
+			String nextWord = st.nextToken();
+			if(nextWord.equals(" ")) { // 공백이면 그대로 출력
+				sb.append(nextWord);
+			}else {
+				sb.append(nextWord.substring(0, 1).toUpperCase() + nextWord.substring(1)); // 첫 번째 요소 대문자로 바꾸고 나머지 이어 붙이기
+			}
+			
+		
+		}
+		sb.toString(); // 반드시 필요
+		System.out.println(sb);
+	}
+}
---
+++ Lv2/최댓값과최솟값.java
@@ -0,0 +1,35 @@
+package Lv2;
+
+import java.util.Scanner;
+
+// 공백으로 입력
+public class 최댓값과최솟값 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+				
+		System.out.println(str);
+		
+		String [] test = str.split(" "); // split 함수 사용하여 공백 기준으로 test 배열에 저장
+		
+		// 첫 번째 요소로 초기화
+		int max , min , r;
+		max = min = Integer.parseInt(test[0]); 
+		
+		for(int i = 1 ; i < test.length ; i++) { // 첫 번째 요소부터 반복
+			r = Integer.parseInt(test[i]); // 공백을 기준으로 자른 각 요소 저장
+			
+			if(max < r)
+				max = r;
+			if(min > r)
+				min = r;
+		}
+		
+		System.out.println(min + " " + max); // 형식에 맞게 출력
+		
+		sc.close(); // 스캐너 닫기
+	}
+
+}
